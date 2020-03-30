4a0393383ab9270beb0973c061a79d77bd7b91ec
liusx <997323529@qq.com>
2020-03-31 00:01:45 +0800

    Initial commit

---
+++ src/test/java/com/lxx1030/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxx1030.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
